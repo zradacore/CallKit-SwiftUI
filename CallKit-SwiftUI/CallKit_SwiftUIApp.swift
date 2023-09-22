2770cecc406f22c695fa425af8b18333c18a9911
zradacore <zradacore@gmail.com>
2023-09-22 14:36:36 +0300

    Initial Commit

---
+++ CallKit-SwiftUI/CallKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallKit_SwiftUIApp.swift
+//  CallKit-SwiftUI
+//
+//  Created by Владимир Никитин on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CallKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
